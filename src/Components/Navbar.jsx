29de6022c7100d42165a2f474967fffc35245f36
gitanushka <77441008+gitanushka@users.noreply.github.com>
2023-03-21 12:02:40 +0530

    feat: navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+`
+
+
+
+const Navbar = () => {
+  return (
+    <Container>
+        navbar
+    </Container>
+  );
+};
+
+export default Navbar;

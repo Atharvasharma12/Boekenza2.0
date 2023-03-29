8e7278b735458ee15c7d324249adaf6a5cefccc2
Atharva sharma <89251910+Atharvasharma12@users.noreply.github.com>
2023-03-29 12:44:48 +0530

    doc : images change

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "WhatsApp Image 2023-03-27 at 22.20.35.jpeg",
-    title: "Boekenza",
-    desc: "Welcome to Boekenza",
+    img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    title: "BOEKENZA",
+    desc: "Welcome to our online stationary store, where we offer a unique selection of pre-loved stationary items that have been gently used by others. We believe in reducing waste and providing our customers with affordable options that are both sustainable and functional.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "Screenshot_20230328-000238_ClearScanner.jpg",
+    img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     title: "AMAZING COLLECTION",
-    desc: "NOTEBOOKS & STATIONARY",
+    desc: "Our collection includes a variety of writing tools, notebooks, journals, and desk organizers that have been thoroughly cleaned and sanitized for your peace of mind. We also carry limited edition and hard-to-find items that are no longer available in stores.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "WhatsApp Image 2023-03-27 at 22.32.39.jpeg",
-    title: "JOIN IN",
-    desc: "NOVELS",
+    img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    title: "Join in",
+    desc: "By purchasing pre-loved stationary items from our store, you're not only saving money but also contributing to a more sustainable future. Join us in our mission to reduce waste and shop our selection of high-quality, second-hand stationary items today!",
     bg: "fbf0f4",
   },
 ];
@@ -25,61 +25,29 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "WhatsApp Image 2023-03-27 at 22.20.35.jpeg",
+    img: "./photos/WhatsApp Image 2023-03-27 at 22.20.35.jpeg",
     title: "ALGORITHMS",
+    desc: "Welcome to our online stationary store, where we offer a unique selection of pre-loved stationary items that have been gently used by others. We believe in reducing waste and providing our customers with affordable options that are both sustainable and functional.",
+    bg: "f5fafd",
   },
   {
     id: 2,
-    img: "WhatsApp Image 2023-03-27 at 22.27.14.jpeg",
-    title: "STATIONARY",
+    img: "./photos/WhatsApp Image 2023-03-27 at 22.27.14.jpeg",
+    title: "ENGINEERING TOOLS",
+    desc: "Our collection includes a variety of writing tools, notebooks, journals, and desk organizers that have been thoroughly cleaned and sanitized for your peace of mind. We also carry limited edition and hard-to-find items that are no longer available in stores.",
+    bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "WhatsApp Image 2023-03-27 at 22.32.39.jpeg",
+    img: "./photos/WhatsApp Image 2023-03-27 at 22.32.39.jpeg",
     title: "NOVELS",
+    desc: "By purchasing pre-loved stationary items from our store, you're not only saving money but also contributing to a more sustainable future. Join us in our mission to reduce waste and shop our selection of high-quality, second-hand stationary items today!",
+    bg: "fbf0f4",
   },
 ];
-    {
-      id: 1,
-      img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      title: "BOEKENZA",
-      desc: "Welcome to our online stationary store, where we offer a unique selection of pre-loved stationary items that have been gently used by others. We believe in reducing waste and providing our customers with affordable options that are both sustainable and functional.",
-      bg: "f5fafd",
-    },
-    {
-      id: 2,
-      img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      title: "AMAZING COLLECTION",
-      desc: "Our collection includes a variety of writing tools, notebooks, journals, and desk organizers that have been thoroughly cleaned and sanitized for your peace of mind. We also carry limited edition and hard-to-find items that are no longer available in stores.",
-      bg: "fcf1ed",
-    },
-    {
-      id: 3,
-      img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      title: "Join in",
-      desc: "By purchasing pre-loved stationary items from our store, you're not only saving money but also contributing to a more sustainable future. Join us in our mission to reduce waste and shop our selection of high-quality, second-hand stationary items today!",
-      bg: "fbf0f4",
-    },
-  ];
 
   
-  export const categories = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "SHIRT STYLE!",
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "LIGHT JACKETS",
-    },
-  ];
+  
 
 export const popularProducts = [
   {
